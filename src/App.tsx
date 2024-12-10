9d7da1faea60e90c87d418e71624def102c5a3dd
Keri Dominik <keridodorobi@gmail.com>
2024-12-10 13:07:14 +0100

    refresh

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,12 @@ function App() {
   const [PostUserLastName, setPostUserLastName] = useState("");
   const [PostUserClass, setPostUserClass] = useState("");
   const [putEmail, setPutEmail] = useState("");
+  const [putEmailNew, setPutEmailNew] = useState("");
+  const [putFirstNameNew, setPutFirstNameNew] = useState("");
+  const [putLastNameNew, setPutLastNameNew] = useState("");
+  const [putClassNew, setPutClassNew] = useState("");
+  const [deleteUser, setDeleteUSer] = useState("")
+
 
   const submit = () => {
     const postUser = {
@@ -37,7 +43,8 @@ function App() {
       .then((response) => {
         switch (response.status) {
           case 201:
-            console.log("User created successfully");
+            console.log("User created successfully")
+            window.location.reload();
             break;
           case 400:
             console.error("Bad request");
@@ -50,9 +57,62 @@ function App() {
         console.error(error);
       });
 
+ 
    
 
 
+  };
+
+  const submitPut = () => {
+    const putUser = {
+      email: putEmailNew,
+      firstName: putFirstNameNew,
+      lastName: putLastNameNew,
+      class: putClassNew,
+    } as User;
+    
+    const id = putEmail;
+    
+    apiClient.put(`/users/${id}`, putUser)
+      .then(response => {
+        switch (response.status) {
+          case 200:
+            console.log('User updated successfully')
+            window.location.reload();
+            break;
+          case 400:
+            console.error('Bad request');
+            break;
+          default:
+            console.error('An error occurred');
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+ 
+  const delUser = () => {
+    const delID = deleteUser
+    apiClient
+      .delete(`/users/${delID}`)
+      .then((response) => {
+        switch (response.status) {
+          case 200:
+            console.log("User deleted successfully")
+            window.location.reload();
+            break;
+          case 404:
+            console.error("User not found");
+            break;
+          default:
+            console.error("An error occurred");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
@@ -98,7 +158,18 @@ function App() {
 
       <div>
         <h3>Felhasználó módosítása: </h3>
-        <input type="email" placeholder="E-mail:"  onChange={(e) => setPutEmail(String(e.target.value))}/>
+        <input type="email" placeholder="Régi E-mail:"  onChange={(e) => setPutEmail(String(e.target.value))}/>
+        <input type="email" placeholder="E-mail:"  onChange={(e) => setPutEmailNew(String(e.target.value))}/>
+        <input type="email" placeholder="Vezetéknév"  onChange={(e) => setPutFirstNameNew(String(e.target.value))}/>
+        <input type="email" placeholder="Keresztnév:"  onChange={(e) => setPutLastNameNew(String(e.target.value))}/>
+        <input type="email" placeholder="Osztály:"  onChange={(e) => setPutClassNew(String(e.target.value))}/>
+        <button onClick={submitPut}>Változtasd</button>
+      </div>
+
+      <div>
+        <h3>Felhasználó törlése: </h3>
+        <input type="email" placeholder="E-mail" onChange={(e) => setDeleteUSer(String(e.target.value))} />
+        <button onClick={delUser}>Töröl</button>
       </div>
     </div>
   );
